8209ea8dc917632da30e6d151a38ec10e387c2aa
whoiskeeks <selasa5cihuy@gmail.com>
2024-06-16 22:47:34 +0700

    Temperature Calculator RevoU -whoiskeeks

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Defining
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convertButton = document.querySelector('.convertButton');
+const resetButton = document.querySelector('.resetButton');
+const changeButton = document.querySelector('.changeButton');
+const C = document.querySelector('.C')
+const F = document.querySelector('.F')
+
+// Change Function
+const swap = () => {
+    if (C.innerHTML === '°C') {
+        C.innerHTML = '°F';
+        F.innerHTML = '°C';
+        converter.placeholder = "Temperature in °F";
+    }else {
+        F.innerHTML = '°F';
+        C.innerHTML = '°C';
+        converter.placeholder = "Temperature in °C";
+    };
+};
+
+// Reset Function
+const reset = () => {
+    converter.value = '';
+    result.innerHTML = '';
+}
+
+// Convert Function
+const convert = () => {
+    if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
+        result.style.color = "black"
+        if (C.innerHTML === '°C') {
+            let value = (converter.value * 9/5) + 32;
+            result.innerHTML = `${converter.value}°C is equal to ${value.toFixed(2)}°F`;
+        } else {
+            let value = (converter.value - 32) * 5/9;
+            result.innerHTML = `${converter.value}°F is equal to ${value.toFixed(2)}°C`;
+        }
+    } else if (converter.value == '') {
+        result.style.color = "#993300"
+        result.innerHTML = 'Enter some number first!';
+    } else {
+        result.style.color = "#993300"
+        result.innerHTML = 'Number input only!';
+    }
+}
+
+changeButton.addEventListener('click', swap);
+resetButton.addEventListener('click', reset);
+convertButton.addEventListener('click', convert);
+
